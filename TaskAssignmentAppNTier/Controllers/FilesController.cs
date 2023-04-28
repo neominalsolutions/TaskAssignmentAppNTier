0a623d4d2e9e08da6981091f21e2824ce6a7f50c
mert.alptekin <mert.alptekin@neomina.com>
2023-04-28 19:30:35 +0300

    Upload File with IFormFile

---
+++ TaskAssignmentAppNTier/Controllers/FilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAssignmentAppNTier.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class FilesController : ControllerBase
+  {
+
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> OnPostUploadAsync(IFormFile file)
+    {
+      
+
+      return Ok();
+    }
+  }
+}
