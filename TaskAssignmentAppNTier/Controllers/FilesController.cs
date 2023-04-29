4b398c87a062127336ff0893a504697538c2be9f
mert.alptekin <mert.alptekin@neomina.com>
2023-04-29 14:51:19 +0300

    FilesController

--- TaskAssignmentAppNTier/Controllers/FilesController.cs
+++ TaskAssignmentAppNTier/Controllers/FilesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IO.Compression;
+using System.Security.Cryptography.Xml;
 
 namespace TaskAssignmentAppNTier.Controllers
 {
@@ -8,13 +11,33 @@ namespace TaskAssignmentAppNTier.Controllers
   public class FilesController : ControllerBase
   {
 
+    class FileResult
+    {
+      public string Base64 { get; set; }
+
+    }
 
     [HttpPost("upload")]
     public async Task<IActionResult> OnPostUploadAsync(IFormFile file)
     {
-      
 
-      return Ok();
+      var filePath = Path.GetTempFileName();
+
+      using (var stream = new MemoryStream())
+      {
+        file.CopyTo(stream);
+        var bytes = stream.ToArray();
+
+        var result = new FileResult();
+        result.Base64 = $"data:{file.ContentType};base64,{Convert.ToBase64String(bytes)}";
+       
+
+        return Ok(
+          result
+          );
+
+      }
+
     }
   }
 }
